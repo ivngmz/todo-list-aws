8e810e7de63a78d00d871c8838aef487278e0882
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-06 19:34:37 +0000

    Modifico linea 113

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -77,9 +77,10 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('Start: test_put_todo_error')
         # Testing file functions
         from src.todoList import put_item
+        from src.todoList import get_item
         # Table mock
         self.assertRaises(Exception, put_item("", self.dynamodb))
-        self.assertRaises(Exception, put_item("", self.dynamodb))
+        self.assertRaises(Exception, get_item("", self.dynamodb))
         print ('End: test_put_todo_error')
 
     def test_get_todo(self):
