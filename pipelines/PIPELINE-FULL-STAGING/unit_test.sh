58ac6121a0b7700b381c47acc8ef4da24b1d7ad2
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-06 19:14:58 +0000

    añado ordenes coverage para obtencion de detalle

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -8,5 +8,8 @@ export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
-coverage report
-coverage xml
+coverage report -m
+coverage xml -o coverage.xml.out
+cat coverage.xml.out
+coverage debug data
+coverage annotate
