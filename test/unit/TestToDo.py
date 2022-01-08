fc4681797fdb7689c76d91a9d39cf0e04bdb1e5a
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-08 20:34:12 +0000

    modifico put_todo levantando excepcion

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -97,8 +97,6 @@ class TestDatabaseFunctions(unittest.TestCase):
         # Table mock
         self.assertRaises(Exception, put_item("", self.dynamodb))
         self.assertRaises(Exception, get_item("", self.dynamodb))
-        self.assertRaises(ClientError, put_item("", self.dynamodb))
-        self.assertRaises(ClientError, get_item("", self.dynamodb))
 
         MSG_TEMPLATE = (
         'An error occurred (400) when calling the put_item '
