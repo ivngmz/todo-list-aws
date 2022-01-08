9a2d7a6c9c4dbf5e53e80a469ccfedb1f05945da
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-08 08:21:11 +0000

    modifico put_todo levantando excepcion

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -83,9 +83,11 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertRaises(Exception, put_item("", self.dynamodb))
         self.assertRaises(Exception, get_item("", self.dynamodb))
         
+        global MSG_TEMPLATE
+        
         MSG_TEMPLATE = (
         'An error occurred (400) when calling the put_item '
-        'operation1:False')
+        'operation1:lse')
         
         with pytest.raises(botocore.exceptions.ClientError("failed",put_item("", self.dynamodb))) as exc_info:
             print("Imprimo Error")
