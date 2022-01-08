27b3ffd97d99edd907d75a2c12c0fff4aabb8790
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-08 10:25:53 +0000

    modifico put_todo levantando excepcion

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -68,7 +68,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         from src.todoList import create_todo_table
         try:
             self.table.delete()
-        except self.table.exceptions.ResourceNotFoundException as exc_info:
+        except self.TestDatabaseFunctions.exceptions.ResourceNotFoundException as exc_info:
             print("Se genero una excepcion de tipo: ResourceNotFoundException: " + str(exc_info))
         self.assertTrue(self.table)  # check if we got a result
         #self.assertTrue(self.table_local)  # check if we got a result
