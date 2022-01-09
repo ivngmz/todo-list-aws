cea8c28cb31393e5e984adc7b0ceb4fd81a4e3b5
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-09 18:32:02 +0000

    modifico todolist URL ENDPOINT_OVERRIDE

--- src/todoList.py
+++ src/todoList.py
@@ -8,12 +8,13 @@ from botocore.exceptions import ClientError
 
 
 def get_table(dynamodb=None):
-    ENDPOINT_OVERRIDE = 'http://dynamodb:8000'
-    # ENDPOINT_OVERRIDE = http://localhost:8000
+    # ENDPOINT_OVERRIDE = 'http://dynamodb:8000'
+    # ENDPOINT_OVERRIDE = 'http://localhost:8000'
+    # URL = os.environ['ENDPOINT_OVERRIDE']
+    URL = 'http://dynamodb:8000'
+    print('URL dynamoDB:'+URL)
     if not dynamodb:
-        URL = os.environ['ENDPOINT_OVERRIDE']
         if URL:
-            print('URL dynamoDB:'+URL)
             boto3.client = functools.partial(boto3.client, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource, 
                                                endpoint_url=URL)
