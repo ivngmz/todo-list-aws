95b344435acbc8a8f81015d33278135ae33bfaba
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-10 08:50:19 +0000

    modifico excepcion

--- src/todoList.py
+++ src/todoList.py
@@ -29,8 +29,8 @@ def get_item(key, dynamodb=None):
             }
         )
 
-    except ClientError as e:
-        print(e.response['Error']['Message'])
+    except Exception as e:
+        raise e
     else:
         print('Result getItem:'+str(result))
         if 'Item' in result:
