7721092046c514db416ca3553dc36d84fb1ecc32
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-11 08:52:31 +0000

    modifico excepcion

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -5,7 +5,7 @@ set -x
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
-export ENDPOINT_OVERRIDE=`echo "http://"$(nslookup $(curl -s  http://checkip.amazonaws.com) | grep name | awk '{print $4}' | sed 's/.$//')`
+export ENDPOINT_OVERRIDE=`echo "http://"$(nslookup $(curl -s http://checkip.amazonaws.com) | grep name | awk '{print $4}' | sed 's/.$//')":8000"`
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
