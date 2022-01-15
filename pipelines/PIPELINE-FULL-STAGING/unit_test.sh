4c70cf23e81c8cde1caca9f01c8a8ee83cd87b53
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-15 10:10:58 +0000

    modifico excepcion

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -8,7 +8,7 @@ docker start eed9128e1f0d
 export DYNAMODB_TABLE=todoUnitTestsTable
 echo "https://"$Stage"-TodosDynamoDbTable"
 #export ENDPOINT_OVERRIDE="${Stage}-TodosDynamoDbTable"
-#"http://127.0.0.1:8000"
+export ENDPOINT_OVERRIDE="http://127.0.0.1:8000"
 nc -vz 127.0.0.1 8000
 echo "Iniciando dynamodb en local ..."
 aws dynamodb create-table --table-name dynamodb --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://localhost:8000
