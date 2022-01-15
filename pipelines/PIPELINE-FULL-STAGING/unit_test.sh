e37242f70b05dc50364615749abd247fe6c7f098
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-15 10:27:38 +0000

    modifico excepcion

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -9,10 +9,12 @@ echo "https://"$Stage"-TodosDynamoDbTable"
 #export ENDPOINT_OVERRIDE="${Stage}-TodosDynamoDbTable"
 export ENDPOINT_OVERRIDE="http://127.0.0.1:8000"
 nc -vz 127.0.0.1 8000
-if [ $? == 0 ]; then
+if [ $? != 0 ]; then
         echo "El contenedor docker de dynamodb no está levantado"
         exit
     fi
+echo "Mostrando tablas ..."
+aws dynamodb list-tables --endpoint-url http://127.0.0.1:8000 --endpoint-url http://localhost:8000 --region us-east-1
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
