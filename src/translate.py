a22cda9e08c49e174d250d2a19d5d207c666281a
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-15 20:43:06 +0000

    Ampliacion con translate

--- src/translate.py
+++ src/translate.py
@@ -3,20 +3,17 @@ import json
 
 translate_client = boto3.client('translate')
 
-
 def lambda_handler(event, context):
     review_text = event['text']
-
+    
     translate_response = translate_client.translate_text(
             Text=review_text,
             SourceLanguageCode='auto',
             TargetLanguageCode='en'
     )
     
-    
     response = {
             "statusCode": 200,
             "body": json.dumps(translate_response)
         }
-
     return response
