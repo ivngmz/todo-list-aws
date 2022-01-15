f47228584be0dd98d0bbe5624dca0e90e617922f
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-15 20:56:46 +0000

    Ampliacion con translate

--- src/translate.py
+++ src/translate.py
@@ -3,7 +3,7 @@ import json
 
 translate_client = boto3.client('translate')
 
-def lambda_handler(event, context):
+def lambda_handler(event,context):
     review_text = event['text']
     translate_response = translate_client.translate_text(
             Text=review_text,
