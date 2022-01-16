fa129ed22d7811a3e76dc538ef974d2c2d24b10c
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-16 10:39:47 +0000

    Ampliacion con translate

--- src/translate.py
+++ src/translate.py
@@ -1,19 +1,47 @@
+import logging
 import boto3
 import json
+import todoList
 
+global translate_client
 translate_client = boto3.client('translate')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    review_text = event['text']
-    translate_response = translate_client.translate_text(
-        Text=review_text,
-        SourceLanguageCode='auto',
-        TargetLanguageCode='en'
-        )
+    logger.info(event)
+    global data
+    data = json.loads(event['body'])
+    logger.info(data)
+    global translate_response
+    translate_response = ""
+    global response
+    
+    try:
+        review_text = data['text']
+        translate_response = translate_client.translate_text(
+            Text=review_text,
+            SourceLanguageCode='auto',
+            TargetLanguageCode='en'
+            )
+        logger.error(translate_response)
+    except KeyError as exc:
+        response = {
+            "statusCode": 417,
+            "body": json.dumps("Error: Key error")
+        }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(translate_response)
+    if (translate_response):
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(translate_response)
+        }
+    elif (response != None):
+        print ("we have a problem")
+    else:
+        response = {
+            "statusCode": 400,
+            "body": json.dumps("Error: Request has not valid JSON")
         }
     return response
