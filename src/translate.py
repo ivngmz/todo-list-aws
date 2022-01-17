838dc2f4bec5a7abce6e64db3260d96dbc86483a
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-17 08:01:27 +0000

    Ampliacion con translate

--- src/translate.py
+++ src/translate.py
@@ -1,6 +1,7 @@
 import logging
 import boto3
 import json
+import todoList
 
 global translate_client
 translate_client = boto3.client('translate')
@@ -10,16 +11,17 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     logger.info(event)
-    global data
-    data = json.loads(event['body'])
-    logger.info(data)
     global translate_response
     translate_response = ""
     global response
 
     try:
-        review_text = data['text']
-        review_language = data['lang']
+        review_id = event['pathParameters']['id']
+        review_language = event['pathParameters']['language']
+        item_response = json.loads(json.dumps(todoList.get_item(review_id)))
+        print(item_response)
+        review_text = item_response["text"]
+        logger.info(review_text)
         translate_response = translate_client.translate_text(
             Text=review_text,
             SourceLanguageCode='auto',
@@ -37,6 +39,7 @@ def lambda_handler(event, context):
             "statusCode": 200,
             "body": json.dumps(translate_response)
         }
+        logger.info(response)
     elif response is not None:
         print("we have a problem")
     else:
--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -111,6 +111,47 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Get TODO')
+
+    def test_api_gettodo_translated(self):
+        print('---------------------------------------')
+        print('Starting - integration test Get TODO translated')
+        #Add TODO
+        url = BASE_URL+"/todos"
+        data = {
+         "text": "Integration text example - GET- Translated"
+        }
+        response = requests.post(url, data=json.dumps(data))
+        json_response = response.json()
+        print('Response Add Todo: '+ str(json_response))
+        jsonbody= json.loads(json_response['body'])
+        ID_TODO = jsonbody['id']
+        print ('ID todo:'+ID_TODO)
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            jsonbody['text'], "Integration text example - GET", "Error en la petición API a {url}"
+        )
+        #Test GET TODO TRANSLATED
+        languages = {"Español":"es", "Frances":"fr", "aleman":"de", "Checo":"cs"}
+        
+        for key, value in languages.items():
+            url = BASE_URL+"/todos/"+ID_TODO+"/"+str(value)
+            response = requests.get(url)
+            json_response = response.json()
+            print('Response Get Todo en '+str(key)+': '+str(json_response))
+            self.assertEqual(
+                response.status_code, 200, "Error en la petición API a {url}"
+            )
+            self.assertEqual(
+                json_response['text'], "Integration text example - GET", "Error en la petición API a {url}"
+            )
+            #Delete TODO to restore state
+            response = requests.delete(url)
+            self.assertEqual(
+                response.status_code, 200, "Error en la petición API a {url}"
+            )
+        print('End - integration test Get TODO Translated')
     
     def test_api_updatetodo(self):
         print('---------------------------------------')
