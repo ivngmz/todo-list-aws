bea0c4395a4532ac8c1d6ed680393e8d48778802
ivngmz2018 <ivngmz2018@gmail.com>
2022-01-29 07:21:37 +0000

    añado region

--- src/todoList.py
+++ src/todoList.py
@@ -12,7 +12,7 @@ def get_table(dynamodb=None):
         URL = os.environ['ENDPOINT_OVERRIDE']
         if URL:
             print('URL dynamoDB:'+URL)
-            boto3.client = functools.partial(boto3.client, endpoint_url=URL)
+            boto3.client = functools.partial(boto3.resource, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
         dynamodb = boto3.resource("dynamodb")
@@ -144,5 +144,4 @@ def create_todo_table(dynamodb):
     table.meta.client.get_waiter('table_exists').wait(TableName=tableName)
     if (table.table_status != 'ACTIVE'):
         raise AssertionError()
-  
     return table
